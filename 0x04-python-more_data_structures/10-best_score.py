cad26ec9f10add79f9402544c15ddfc336f44714
Asmelash1 <abrihamasmelash@gmail.com>
2022-08-31 10:41:49 -0700

    task ten

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,10 +2,8 @@
 
 def best_score(a_dictionary):
 
-    if not a_dictionary:
+    if a_dictionary is None or a_dictionary == {}:
 
-        return (None)
+        return None
 
-
-
-    return (max(a_dictionary, key=a_dictionary.get))
+    return max(a_dictionary.items(), key=lambda n: n[1])[0]
