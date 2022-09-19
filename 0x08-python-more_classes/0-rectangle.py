5c9d1ae9ff9d118c100d54990c610e5b911cd5e3
Asmelash1 <abrihamasmelash@gmail.com>
2022-09-19 09:54:04 -0700

    t00

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 
-"""Defines a Rectangle class."""
+"""
+
+0-rectangle: class Rectangle
+
+"""
+
+
 
 
 
 class Rectangle:
 
-    """Represent a rectangle."""
+    """ Empty class """
 
     pass
