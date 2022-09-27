af4b734067891517729085f56bbd34c3d27ead4c
Asmelash1 <abrihamasmelash@gmail.com>
2022-09-27 08:29:51 -0700

    t1e

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-
-"""Contains `MyList` class"""
-
-
-
+"""
+    1-my_list: class MyList
+"""
 
 
 class MyList(list):
-
-    """Class that extends the list base class"""
-
-
-
+    """
+        This class inherits from list.
+        Attributes:
+        Methods:
+            print_sorted - prints the list in ascending order
+    """
     def print_sorted(self):
-
-        """Prints a sorted list"""
-
+        """
+           prints a list in ascending order.
+        """
         print(sorted(self))
